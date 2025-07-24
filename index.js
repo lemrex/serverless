6666d9c01b9c445836bc80fd2d780530c4c1794d
hexinli_int/emeka.raphael <emeka.ndubuisi.raphael@huawei.com>
2025-07-24 14:58:34 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+exports.handler = async function (event, context) {
+    const now = new Date();
+
+    return {
+        statusCode: 200,
+        isBase64Encoded: false,
+        headers: { "Content-Type": "application/json" },
+        body: {
+            message: "Current date and time",
+            datetime: now.toISOString()
+        },
+    };
+};
